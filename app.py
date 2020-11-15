d7f27086c7dc80c90dcac79d5ef1624a9587541f
Ekanpat <ekanpat83@gmail.com>
2020-11-14 21:28:23 -0600

    Mission to Marsh Web Scrapping

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+# Set up flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Set Up App Routes
+##define the route for the HTML page.
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+# add the next route and function to the code
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+# Run flask
+if __name__ == "__main__":
+   app.run()
